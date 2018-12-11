aeebc697f53bd083cd39c72d3ce824546d0ca980
NFalinska <nataliia.falinska@gmail.com>
2018-12-11 13:38:49 +0200

    some fix

--- css-practice/task_3/header.js
+++ css-practice/task_3/header.js
@@ -1,6 +1,6 @@
 /* global document, window, pageYOffset */
-const header = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
+  const header = document.getElementsByTagName('nav')[0];
   pageYOffset > 0 ? header.classList.add('fixed-header')
 : header.classList.remove('fixed-header');
 });
